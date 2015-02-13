e9f6eba10847b77a18c7e974f0a8a83d8fc47ff7
Abdietou <abdietou@gmail.com>
2015-02-13 17:21:46 +0100

    Modif 2 Entity

--- Agora2015/src/Modele/OuvrierEntity.java
+++ Agora2015/src/Modele/OuvrierEntity.java
@@ -1,5 +1,16 @@
 package Modele;
 
 public class OuvrierEntity {
+	
+	private String ID;
+	private String Nom;
+	private String Prenom;
+	private String Login;
+	private String Password;
+	private Integer Note ;
+	private boolean Active ;
+	private DisponibiliteEntity disponibilite;
+	private PrestationEntity prestation;
+	private PrixEntity prix;
 
 }
